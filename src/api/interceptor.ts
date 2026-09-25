5d31a080b009dc43626e35f1600a22a40307258b
Fatemeh <fatemeh.hosseini838383@gmail.com>
2026-09-25 21:22:57 +0330

    add interceptor file from Axios

---
+++ src/api/interceptor.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { CloudLightning } from 'lucide-react';
+const productAxios = axios.create({
+    baseURL: 'https://dummyjson.com/',
+    timeout: 1000,
+    headers: { 'Content-Type': 'application/json' },
+
+})
+
+productAxios.interceptors.response.use(
+    (response) => (response),
+
+  (error)=>{
+    if(error.response?.status===401){
+        console.log("unAuthorized");
+    }
+
+    if(error.response?.status===500){
+        console.log("server Error");
+    }
+
+    if(error.response?.status===400){
+        console.log("not found");
+    }
+
+    return Promise.reject(error)
+  }
+);
